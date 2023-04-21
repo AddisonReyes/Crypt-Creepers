71fa381573e091a8742e699318229e5521c08376
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2023-04-21 13:19:13 -0400

    Create player and aim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed = 0.0f;
+    [SerializeField] Transform aim;
+    [SerializeField] Camera camera;
+
+    float horizontal;
+    float vertical;
+
+    Vector3 moveDirection;
+    Vector2 facingDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Player Movement
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        moveDirection.x = horizontal;
+        moveDirection.y = vertical;
+        transform.position += moveDirection * Time.deltaTime * speed;
+
+        // Aim Movement
+        facingDirection = camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        aim.position = transform.position + (Vector3)facingDirection.normalized;
         
     }
 }
